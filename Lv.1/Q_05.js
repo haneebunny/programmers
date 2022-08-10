76d4fc83ca33b497b8257b392164bdba6cee6b5f
eunchae <104378330+dev-eun-chae@users.noreply.github.com>
2022-08-10 19:25:59 +0900

    Q 05 짝수와 홀수

---
+++ Lv.1/Q_05.js
@@ -0,0 +1,30 @@
+// 평균 구하기
+// 문제 설명
+// 정수를 담고 있는 배열 arr의 평균값을 return하는 함수, solution을 완성해보세요.
+
+// 제한사항
+// arr은 길이 1 이상, 100 이하인 배열입니다.
+// arr의 원소는 -10,000 이상 10,000 이하인 정수입니다.
+
+// 1. 반복문
+
+function solution(arr) {
+  let sum = 0;
+  for (i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+
+  const average = sum / arr.length;
+  return average;
+}
+
+// 2. reduce
+
+function solution(arr) {
+  const sum = arr.reduce((acc, cur) => {
+    return acc + cur;
+  }, 0);
+  return sum / arr.length;
+}
+
+// reduce() : reduce() 메서드는 배열의 각 요소에 대해 주어진 리듀서(reducer) 함수를 실행하고, 하나의 결과값을 반환한다.
